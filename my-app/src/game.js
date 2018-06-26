99ac825c1b78ddf2f26f53925b54684ba0c01544
Malachi Irwin <malachi@theirwins.ws>
2018-06-25 21:59:37 -0500

    deleted commented out code

--- my-app/src/game.js
+++ my-app/src/game.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class Game {
- //  componentDidMount(){
- //
- //   fetch('http://localhost:4567/game', {
- //     method: 'get'
- //   }).then((data) => {
- //     game: this.game = data
- //   });
- //
- // }
+class Game extends React.Component {
   render() {
     return (
       <div className="Game">
-        // <header className="Game-header">
-        //   <img src="cards/ha.png" className="Game-logo" alt="logo" />
-        //   <h1 className="App-title">Go fish</h1>
-        // </header>
+        <header className="Game-header">
+          <img src="cards/ha.png" className="Game-logo" alt="logo" />
+          <h1 className="App-title">Go fish</h1>
+        </header>
         <h1>Hello World</h1>
-        // <h1>{this.game}</h1>
       </div>
     );
   }
