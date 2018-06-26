fd5a6b93cfaf500f78bdf833b295509a1a5709d0
Malachi Irwin <malachi@theirwins.ws>
2018-06-25 19:32:11 -0500

    create game.js

---
+++ my-app/src/game.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Game {
+ //  componentDidMount(){
+ //
+ //   fetch('http://localhost:4567/game', {
+ //     method: 'get'
+ //   }).then((data) => {
+ //     game: this.game = data
+ //   });
+ //
+ // }
+  render() {
+    return (
+      <div className="Game">
+        // <header className="Game-header">
+        //   <img src="cards/ha.png" className="Game-logo" alt="logo" />
+        //   <h1 className="App-title">Go fish</h1>
+        // </header>
+        <h1>Hello World</h1>
+        // <h1>{this.game}</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
