d5bd01bf1cf3fe133b594ade5a8b6b66d3fe5df6
Malachi Irwin <malachi@theirwins.ws>
2018-06-26 22:28:52 -0500

    make it give random names to robots

--- my-app/react.rb
+++ my-app/react.rb
@@ -7,160 +7,45 @@ require "./../lib/game"
 require "json"
 require "sinatra/json"
 
-$clients = []
-$pending_clients = []
-$game = nil
-$results = []
-$counter = 0
-
 class App < Sinatra::Base
   MESSAGE_KEY = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt.random_key
   NUMBER_OF_PLAYERS = 4
+  russain_american_names = ["Aleksandr", "Aleksey", "Alex", "Alexander", "Alexandr", "Alexandra", "Alexey", "Alexis", "Alper", "Andrej", "Andrew", "Andrey", "Anton", "Anya", "Artem", "Arthur", "Artur", "Artym", "Artyom", "Aytal", "Baldan", "Borislav", "Collosus", "Dan", "Daniel", "Daniil", "Danilov", "Dave", "Denis", "Dima", "Dmitrii", "Dmitriy", "Dmitry", "Dominique", "Donatien", "Edward", "Fabrice", "Fayad", "Gena", "Gleb", "Grairik", "Grechihckin", "Hamza", "Igor", "Ilushka", "Ilya", "Ivan", "Kirill", "Kirill", "Nikonov", "Kolya", "Konstantin", "Kostya", "Lex", "Max", "Maxim", "Maximka", "Mikhail", "Murad", "Nastya", "Nick", "Nicky", "Nike", "Nikita", "Oleg", "Omer", "Ostin", "Pasha", "Paul", "Pavel", "Pawel", "Philip", "Raoul", "Rodion", "Roma", "Roman", "Ruslan", "Sacha", "Samuele", "Sasha", "Sebastian", "Serge", "Sergei", "Sergey", "Stas", "Thierry", "Tseden", "Uche", "Vadim", "Vadmas", "Valera", "Vanya", "Vasiliy", "Vlad", "Vladimir", "Vladislav", "Vova", "Will", "Yegor", "Yura", "Yuri", "Aigul", "Aleksandra", "Alena", "Alex", "Alexandra", "Alice", "Alina", "Alisa", "Alla", "Alyona", "Anastasia", "Anastasiya", "Ann", "Anna", "Anny", "Anya", "Arina", "Catherine", "Chimita", "Christine", "Daria", "Darya", "Dasha", "Diana", "Dinara", "Ekaterina", "Elena", "Evgenia", "Galina", "Galya", "Gulnaz", "Guzel", "Helen", "Inna", "Irene", "Irina", "Jane", "Julia", "Julie", "Karina", "Kate", "Katerina", "Katia", "Katy", "Katya", "Kristina", "Ksenia", "Kseniya", "Ksusha", "Lana", "Lena", "Lera", "Liliya", "Lina", "Lisa", "Liza", "Luba", "Lyuba", "Margarita", "Maria", "Marie", "Marina", "Mariya", "Mary", "Masha", "Nadya", "Nastia", "Nastya", "Natalia", "Natalie", "Nataly", "Natalya", "Natasha", "Nina", "Oksana", "Olesya", "Olga", "Olya", "Pauline", "Polina", "Regina", "Sasha", "Sofia", "Sonya", "Sveta", "Svetlana", "Tanya", "Tatiana", "Tatyana", "Valentina", "Valeria", "Valerie", "Varya", "Vera", "Veronika", "Victoria", "Vika", "Yana", "Yulia", "Zhanna"]
+  array_of_names = ["Sophia", "Jackson", "Olivia", "Liam", "Emma", "Noah", "Ava", "Aiden", "Isabella", "Lucas", "Mia", "Caden", "Aria",	"Grayson", "Riley",	"Mason", "Zoe", "Elijah", "Amelia",	"Logan", "Layla",	"Oliver", "Charlotte",	"Ethan", "Aubrey",	"Jayden", "Lily",	"Muhammad", "Chloe",	"Carter", "Harper",	"Michael", "Evelyn",	"Sebastian", "Adalyn",	"Alexander", "Emily",	"Jacob", "Abigail",	"Benjamin", "Madison",	"James", "Aaliyah",	"Ryan", "Avery",	"Matthew", "Ella",	"Daniel", "Scarlett",	"Jayce", "Maya",	"Mateo", "Mila",	"Caleb", "Nora",	"Luke", "Camilla",	"Julian", "Arianna",	"Jack", "Eliana",	"William", "Hannah",	"Wyatt", "Leah",	"Gabriel", "Ellie",	"Connor", "Kaylee",	"Henry", "Kinsley",	"Isaiah", "Hailey",	"Isaac", "Madelyn",	"Owen", "Paisley",	"Levi", "Elizabeth",	"Cameron", "Addison",	"Nicholas", "Isabelle",	"Josiah", "Anna",	"Lincoln", "Sarah",	"Dylan", "Brooklyn",	"Samuel", "Mackenzie", "John", "Victoria",	"Nathan", "Luna", "Leo", "Penelope", "David", "Grace", "Adam", "Davy"]
 
+  russain_names = ["Абрам", "Александр", "Алексей", "Альберт", "Анатолий", "Андрей", "Антон", "Аркадий", "Арсений", "Артём", "Артур", "Афанасий", "Богдан", "Борис", "Вадим", "Валентин", "Валерий", "Василий", "Вениамин", "Виктор", "Виталий", "Влад", "Владимир", "Владислав", "Всеволод", "Вячеслав", "Гавриил", "Гарри", "Геннадий", "Георгий", "Герасим", "Герман", "Глеб", "Григорий", "Давид", "Даниил", "Денис", "Дмитрий", "Евгений", "Егор", "Ефим", "Захар", "Иван", "Игнат", "eegNAH", "Игорь", "Илларион", "Илья", "Иммануил", "Иосиф", "Кирилл", "Константин", "Лев", "Леонид", "Макар", "Максим", "Марат", "Марк", "Матвей", "Михаил", "Нестор", "Никита", "Николай", "Олег", "Павел", "Пётр", "Роберт", "Родион", "Роман", "Ростислав", "Руслан", "Семён", "Сергей", "Спартак", "Станислав", "Степан", "Тарас", "Тимофей", "Тимур", "Трофим", "Эдуард", "Эрик", "Юлиан", "Юрий", "Яков", "Ярослав"]
   post('/join') do
     json_obj = JSON.parse(request.body.read)
-    $clients.push(json_obj["name"])
-    $game ||= GofishGame.new
+    number_of_players = json_obj["number_of_players"].to_i
+    $game = GofishGame.new
     if $game.players
       $game
     else
-      $game.start(4, [json_obj["name"], "Jimmy", "Timmy", "Limmy"])
+      names = []
+      numbers_used = []
+      (number_of_players - 1).times do
+        random_number = rand(russain_names.length)
+        name = russain_names.fetch(random_number)
+        until name != json_obj["name"]
+          numbers_used.each do |number|
+            if number == random_number
+              random_number = rand(russain_names.length)
+            end
+          end
+          name = russain_names.fetch(random_number)
+        end
+        numbers_used.push(random_number)
+        names.push(russain_names.fetch(random_number))
+      end
+      names.push(json_obj["name"])
+      $game.start(number_of_players, names)
     end
-    json {status: 200, message: "Hi #{json_obj["name"]}"}
+    hash = {status: 200, message: "Hi #{json_obj["name"]}"}
+    json hash
   end
 
   get "/game" do
-    json {game: $game}
-  end
-
-  private
-
-  def client_number
-    @client_number ||= decrypt_client_number(params['client_number'])
-  end
-
-  def client_name
-    @client_name ||= decrypt_client_name(params["name"])
-  end
-
-  def encrypt_client_number(number)
-    "hello-#{number}-dolly".encrypt(MESSAGE_KEY)
-  end
-
-  def encrypt_client_name(name)
-    name.encrypt(MESSAGE_KEY)
-  end
-
-  def add_waiting_players
-    client = $pending_clients.shift(1)[0]
-    $clients.push(client)
-  end
-
-  def decrypt_client_number(text)
-    text.decrypt(MESSAGE_KEY).split('-')[1].to_i
-  end
-
-  def decrypt_client_name(text)
-    text.decrypt(MESSAGE_KEY).split('-')[0]
+    game = {game: $game}
+    json game
   end
 end
-
-#
-# get("/waiting") do
-#   if $clients.length == NUMBER_OF_PLAYERS
-#     if $message == "yes"
-#       $message = "no"
-#       pusher_client.trigger('app', 'Game-is-starting', {message: 'Game is starting'})
-#     end
-#     $game ||= GofishGame.new
-#     if $game.players == nil
-#       $game.start(NUMBER_OF_PLAYERS, $clients)
-#     end
-#     $result = "The game is starting"
-#     redirect("/playing_game?name=#{encrypt_client_name client_name}")
-#   else
-#     slim(:waiting)
-#   end
-# end
-#
-# get "/please_wait" do
-#   if $message2 == "yes"
-#     $message2 = "no"
-#     add_waiting_players
-#     redirect("/waiting?name=#{encrypt_client_name client_name}")
-#   else
-#     slim :please_wait
-#   end
-# end
-#
-# post "/playing_game" do
-#   if client_name == $game.player_who_is_playing.name
-#     @turn = true
-#   end
-#   request = params["request"]
-#   regex = /ask\s(\w+).*\s(\w{2}|\w{1})/i
-#   if request.match(regex)
-#     matches = request.match(regex).captures
-#     if matches[0] == $game.players[$game.player_turn - 1].name
-#       redirect("/playing_game?name=#{encrypt_client_name client_name}")
-#     else
-#       request = Request.new(client_name, matches[0], matches[1])
-#       result = $game.do_turn(request)
-#       if result == "you can't ask that"
-#         return redirect("/playing_game?name=#{encrypt_client_name client_name}")
-#       end
-#       @player = $game.find_player_by_name(client_name)
-#       @other_players = $game.players.reject { |player| player.name == client_name }
-#       $message = "yes"
-#       if result == "Go fish"
-#         final_result = "#{client_name} asked #{matches[0]} for a #{matches[1]} but #{matches[0]} did not have one"
-#       else
-#         final_result = "#{matches[0]} gave #{client_name} the #{result}"
-#       end
-#       $results.push(final_result)
-#       redirect("/playing_game?name=#{encrypt_client_name client_name}")
-#     end
-#   else
-#     redirect("/playing_game?name=#{encrypt_client_name client_name}")
-#   end
-# end
-#
-# get("/playing_game") do
-#   if $message == "yes"
-#     $message = "no"
-#     pusher_client.trigger('app', 'next-turn', {message: "next turn"})
-#   end
-#   if $game.winner
-#     if $counter == 0
-#       $counter = 1
-#       pusher_client.trigger('app', 'next-turn', {message: "Game has ended"})
-#     end
-#   end
-#   if client_name == $game.player_who_is_playing.name
-#     @turn = true
-#   end
-#   @player = $game.find_player_by_name(client_name)
-#   @other_players = $game.players.reject { |player| player.name == client_name }
-#   if $counter == 1
-#     $all_clients_gone = "yes"
-#     redirect "/ended"
-#   else
-#     slim(:go_fish)
-#   end
-# end
-#
-# get "/ended" do
-#   if $all_clients_gone == "yes"
-#     $winning_message = $game.winner
-#     $all_clients_gone = "game stuff is gone"
-#     sleep(1)
-#     $game = nil
-#     $clients = []
-#     $results = []
-#     $counter = 0
-#     $message2 = "yes"
-#     pusher_client.trigger('app', 'welcome-waiting-players', {message: 'waiting to waiting'})
-#   end
-#   slim(:game_end)
-# end
-#
