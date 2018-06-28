8ba6378db9e6e848cc5e0b2d761fb685a6cc2ab4
Malachi Irwin <malachi@theirwins.ws>
2018-06-27 21:11:30 -0500

    Made bot render even without matches

--- my-app/src/components/bot.js
+++ my-app/src/components/bot.js
@@ -4,21 +4,81 @@ import '../App.css';
 class RobotPlayer extends React.Component {
   render() {
     if(this.props.bot.matches !== []) {
-      return (
-        <div className="robotPlayer">
-          <div>
-            <h1 key={'mykey' + this.props.index}>{this.props.name}</h1>
-            {this.props.bot.hand.map((card, index) =>
-              <img key={'mykey' + index} className="hand" src="cards/backs_custom.jpg" alt=""/>
-              )
-            }
-            {this.props.bot.matches.map((card, index) =>
-              <img key={'mykey' + index} className="matches" src={`cards/${card.to_img_path}.png`} alt=""/>
-              )
-            }
+      if(this.props.turn === true) {
+        if(this.props.targetPlayerValue === this.props.bot.name){
+          return (
+            <div className="robotPlayer" onClick={() => {this.props.targetPlayer(this.props.bot.name)}}>
+              <div>
+                <h1 key={'mykey' + this.props.index} className="highlight">{this.props.name}</h1>
+                {this.props.bot.hand.map((card, index) =>
+                  <img key={'mykey' + index} className="hand" src="cards/backs_custom.jpg" alt=""/>
+                  )
+                }
+                <div>
+                  {this.props.matches.map((match, index) =>
+                    <div className="matches">
+                      <span className="inbetween-match"/>
+                      {match.map((card, index) =>
+                        <img key={'mykey' + index} className="match" src={`cards/${card.to_img_path}.png`} alt=""/>
+                        )
+                      }
+                    </div>
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+          );
+        }else{
+          return (
+            <div className="robotPlayer" onClick={() => {this.props.targetPlayer(this.props.bot.name)}}>
+              <div>
+                <h1 key={'mykey' + this.props.index}>{this.props.name}</h1>
+                {this.props.bot.hand.map((card, index) =>
+                  <img key={'mykey' + index} className="hand" src="cards/backs_custom.jpg" alt=""/>
+                  )
+                }
+                <div>
+                  {this.props.matches.map((match, index) =>
+                    <div className="matches">
+                      <span className="inbetween-match"/>
+                      {match.map((card, index) =>
+                        <img key={'mykey' + index} className="match" src={`cards/${card.to_img_path}.png`} alt=""/>
+                        )
+                      }
+                    </div>
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+          );
+        }
+      }else{
+        return (
+          <div className="robotPlayer">
+            <div>
+              <h1 key={'mykey' + this.props.index}>{this.props.name}</h1>
+              {this.props.bot.hand.map((card, index) =>
+                <img key={'mykey' + index} className="hand" src="cards/backs_custom.jpg" alt=""/>
+                )
+              }
+              <div>
+                {this.props.matches.map((match, index) =>
+                  <div key={'mykey' + index} className="matches">
+                    <span className="inbetween-match"/>
+                    {match.map((card, index) =>
+                      <img key={'mykey' + index} className="match" src={`cards/${card.to_img_path}.png`} alt=""/>
+                      )
+                    }
+                  </div>
+                  )
+                }
+              </div>
+            </div>
           </div>
-        </div>
-      );
+        );
+      }
     }else{
       return (
         <div className="robotPlayer">
@@ -28,6 +88,18 @@ class RobotPlayer extends React.Component {
               <img key={'mykey' + index} className="hand" src="cards/backs_custom.jpg" alt=""/>
               )
             }
+            <div>
+              {this.props.matches.map((match, index) =>
+                <div className="matches">
+                  <span className="inbetween-match"/>
+                  {match.map((card, index) =>
+                    <img key={'mykey' + index} className="match" src={`cards/${card.to_img_path}.png`} alt=""/>
+                    )
+                  }
+                </div>
+                )
+              }
+            </div>
           </div>
         </div>
       )
