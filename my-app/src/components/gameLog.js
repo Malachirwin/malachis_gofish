b935f55d60aee5015f78bfae74b87d307c5619d5
Malachi Irwin <malachi@theirwins.ws>
2018-06-28 08:50:06 -0500

    Made gamelog hide

--- my-app/src/components/gameLog.js
+++ my-app/src/components/gameLog.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import '../App.css';
-import ReactDOM from 'react-dom';
+
+class Log extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.logs.map((book, index) =>
+          <h4 className="book" key={`books${index}`}>{book}</h4>
+        )}
+      </div>
+    )
+  }
+}
+
 class GameLog extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {showResults: false}
+    this.state = {}
   }
   whenClick() {
-    this.setState({showResults: true})
+    if(this.state.showResults === false) {
+      this.setState({showResults: true})
+    }else{
+      this.setState({showResults: false})
+    }
   }
   render() {
     return (
       <div className="log">
         <div>
-            <input type="submit" value="-Game Log-" onClick={this.whenClick.bind(this)} />
-            { this.state.showResults ? <div>
-                {this.props.log.slice(0).reverse().slice(0, 15).map((book, index) =>
-                  <h4 className="book" key={`books${index}`}>{book}</h4>
-                )}
-              </div> : null
+            <h1 className="less-top-margin" onClick={this.whenClick.bind(this)}>-Game Log-</h1>
+            { this.state.showResults ? <Log logs={this.props.log.slice(0).reverse().slice(0, 15)}/> : null
             }
         </div>
       </div>
