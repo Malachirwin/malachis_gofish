7f808218e08188bf28952f7c66b37cbe71644a0d
Malachi Irwin <malachi@theirwins.ws>
2018-06-27 21:10:03 -0500

    Made App render the game after a request

--- my-app/src/components/app.js
+++ my-app/src/components/app.js
@@ -1,28 +1,54 @@
 import Join from "./join";
 import React from 'react';
 import Game from './game';
+import EndGame from './endGame';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {value: "join"}
   }
-  updateState() {
-    this.setState({value: "game"})
+  updateState(str) {
+    this.setState({value: str})
+  }
+  componentDidMount() {
+    fetch("/app", {
+      method: "get"
+    }).then(data => data.json()).then(game => {
+      fetch("/winner", {
+        method: "get"
+      }).then(data => data.json()).then(object => {
+        if(object.result === "no winner yet") {
+          if(game["game"] === true) {
+            this.setState({value: "game"})
+          }else{
+            this.setState({value: "join"})
+          }
+        }else{
+          this.setState({value: "endGame"})
+        }
+      })
+    })
   }
   render() {
     if(this.state.value === "game") {
       return (
         <div className="App">
-          <Game/>
+          <Game updateState={this.updateState.bind(this)}/>
         </div>
-      );
-    }else{
+      )
+    }else if(this.state.value === "join"){
       return (
         <div className="App">
           <Join updateState={this.updateState.bind(this)}/>
         </div>
-      );
+      )
+    }else{
+      return (
+        <div className="App">
+          <EndGame updateState={this.updateState.bind(this)}/>
+        </div>
+      )
     }
   }
 }
